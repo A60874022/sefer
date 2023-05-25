cd67611cf6554b72e35834b7e6ff86f4571e9ad8
Alexandr Sharganov <allinshelter@gmail.com>
2023-05-25 20:17:19 +0400

    start admin panel

--- transcribe_app/transcription/models.py
+++ transcribe_app/transcription/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Trancription(models.Model):
     """Класс транскрипции текста."""
-    text = models.CharField()
+    text = models.TextField()
     keywords = models.ManyToManyField('Keywords')
     personalities = models.ManyToManyField('Personalities')
 
