e697eea498022d579ca2bdf3fbaea13c4bb37da9
Александр Ячменев <yachmenyov.alex@yandex.ru>
2024-02-05 18:08:50 +0700

    Correct HOST name variable.

--- transcribe_app/transcribe_app/settings.py
+++ transcribe_app/transcribe_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG'].lower() == 'true'
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
+ALLOWED_HOSTS = os.environ['HOSTS'].split(',')
 
 
 # Application definition
