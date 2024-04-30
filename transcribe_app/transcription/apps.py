2172e2d3d3b4725f0a81a3eaecce1cb13058e4d4
Vladislav Yaremenko <vlad.yaremenko.98@yandex.ru>
2024-04-30 21:48:11 +0300

    Correct verbose name.

--- transcribe_app/transcription/apps.py
+++ transcribe_app/transcription/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class TranscriptionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "transcription"
-    verbose_name = "Транскрипция"
+    verbose_name = _("Транскрипция")
