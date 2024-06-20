a0716489a41367f1cef16d6a6125c39c5b4038d9
Anton <A60874022@yandex.ru>
2024-06-20 19:38:52 +0500

    feat: pre-commit

--- transcribe_app/users/authentication.py
+++ transcribe_app/users/authentication.py
@@ -2,7 +2,6 @@ from rest_framework.authentication import TokenAuthentication
 
 from .models import CastomToken
 
-
 class CastomTokenAuthentication(TokenAuthentication):
     """Переназначение модели токена."""
 
