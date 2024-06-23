46004c27edbe4f78ee852a2ba81def59bdc7db86
Anton <A60874022@yandex.ru>
2024-06-23 11:44:31 +0500

    3

--- transcribe_app/api/views.py
+++ transcribe_app/api/views.py
@@ -79,6 +79,7 @@ class TranscriptionViewSet(ModelViewSet):
     def create_transcription(self, request, pk=None):
         transcription = get_object_or_404(Transcription, pk=pk)
         transcription.audio_url = create_bucket_url(pk)
+        print(transcription.audio_url )
         transcription.audio =  get_audio_file(pk)
         text = create_transcription(pk)
         TextBlock.objects.bulk_create(
